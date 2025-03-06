bf3efe59f5d6b325aa5c9e286840fd5db1d2be91
Alberto107 <agil8518@GMAIL.COM>
2025-03-06 18:29:45 -0300

    include_required

---
+++ 4_include_require/index.php
@@ -0,0 +1,21 @@
+<?php include './header.php';?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center align-items-center flex-column" style="min-height: 80vh;"> 
+            <h1>As 7 maravilhas do mundo moderno</h1>
+            <h3>Conheça as 7 maravilhas do mundo e seus respectivos países</h3>
+            <button class="btn btn-primary">Saiba mais</button>
+        </div>
+    </div>
+</div>
+
+<?php include './footer.php';?>
+
+<!-- Align (alinha verticalmente)
+     Justify Content(alinha horizontalmente) -->
+
+     <!-- O display flex deixa tudo numa linha só, mas o flex direction pode direçionar que tipo de flex vai ser
+     o flex direction por padrão é em row (uma linha só), mas pode mudar -->
+
+     <!-- btn -> btn primary (muda on tipo do botão) -->
