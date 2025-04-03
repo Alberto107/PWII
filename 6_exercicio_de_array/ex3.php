1690f6afce355488427ce2b7e354aeb40ceddab3
Alberto107 <agil8518@gmail.com>
2025-04-03 14:47:21 -0300

    revisando:

--- 6_exercicio_de_array/ex3.php
+++ 6_exercicio_de_array/ex3.php
@@ -30,40 +30,50 @@ $escola_Etec = [
         'nome_do_curso'
     ],
     [
-        'nome_da_matéria'
+        'nome_da_matéria' => 'Programação Mobile',
+        'modulos' => 'I e II'
+    ],
+    [
+        'nome_da_matéria' => 'Programação Web' ,
+        'modulos' => 'I e II e III e IV'
+    ],
+    [
+        'nome_da_matéria' => 'Técnicas avançadas de Programação de Algoritmos',
+        'modulos' => ''
+    ],
+    [
+        'nome_da_matéria' => 'Inglês',
+        'modulos' => 'I e II e III'
+    ],
+    [
+        'nome_do_curso' => 'Administração'
+    ],
+    [
+        'nome_da_matéria' => 'Recursos Humanos',
+        'modulos' => 'I e II'
+    ],
+    [
+        'nome_da_matéria' => 'Gestão de pessoas',
         'modulos'
     ],
     [
-        'nome_da_matéria'
+        'nome_da_matéria' => 'Teoria da Administração',
         'modulos'
     ],
     [
-        'nome_da_matéria'
-        'modulos'
-    ],
-    [
-        'nome_da_matéria'
-        'modulos'
-    ],
-    [
-        'nome_do_curso'
-    ],
-    [
-        'nome_da_matéria'
-        'modulos'
-    ],
-    [
-        'nome_da_matéria'
-        'modulos'
-    ],
-    [
-        'nome_da_matéria'
-        'modulos'
-    ],
-    [
-        'nome_da_matéria'
-        'modulos'
+        'nome_da_matéria' => 'Inglês',
+        'modulos' => 'I apenas'
     ]
 ];
 
+
+ foreach($escola_Etec as $key => $value){
+    echo 'Nome do curso: ' . $value['nome_do_curso'] . '<br>';
+    echo '<br>';
+    echo 'Nome da matéria: ' . $value['nome_da_matéria'] . '<br>';
+    echo '<br>';
+    echo '<br>';
+    echo 'Módulos: ' . $value['modulos'] . '<br>';
+    echo '<br>';
+   }
 ?>
